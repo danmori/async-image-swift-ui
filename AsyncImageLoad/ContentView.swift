7d2f9990d313477a56a9a57a3ae2b523af5cfeb7
Dan Mori <danielstelzer@gmail.com>
2022-07-10 11:41:41 -0300

    Added async image with placeholder

--- AsyncImageLoad/ContentView.swift
+++ AsyncImageLoad/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(minWidth: 120)
+                .foregroundColor(.purple)
+                .opacity(0.5)
+        }
+        padding(40)
     }
 }
 
