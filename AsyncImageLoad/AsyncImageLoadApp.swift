d2c9099e1b10a520f842222e7eca822477412ab6
Dan Mori <danielstelzer@gmail.com>
2022-07-10 09:18:25 -0300

    Initial Commit

---
+++ AsyncImageLoad/AsyncImageLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageLoadApp.swift
+//  AsyncImageLoad
+//
+//  Created by Dan Mori on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
